62623633853a9bbe65cb737d5d83ec0608dd342c
Yavuz Özbay <ozbayavz@gmail.com>
2023-07-04 15:31:43 +0200

    Updates

---
+++ precache-manifest.654d6f7d902e6b1c1c192a219abe4ef4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffb4adb43a6d8625c69a10fd822e712",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "eeb69c06d58e574fe510",
+    "url": "/react-portfolio/static/css/main.cf3c6029.chunk.css"
+  },
+  {
+    "revision": "aa55b90df6e86b943de1",
+    "url": "/react-portfolio/static/js/2.92df2227.chunk.js"
+  },
+  {
+    "revision": "4fb2ddd01bbdb0199af7f68cae9146c9",
+    "url": "/react-portfolio/static/js/2.92df2227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afbf91279c17ab07034",
+    "url": "/react-portfolio/static/js/3.1ce263c2.chunk.js"
+  },
+  {
+    "revision": "eeb69c06d58e574fe510",
+    "url": "/react-portfolio/static/js/main.0673cb2a.chunk.js"
+  },
+  {
+    "revision": "28208ef86c79130c173a",
+    "url": "/react-portfolio/static/js/runtime-main.82a33480.js"
+  },
+  {
+    "revision": "55a1e6c4a0ba70216b36ef3562feddd1",
+    "url": "/react-portfolio/static/media/67596512.55a1e6c4.jpeg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/react-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/react-portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/react-portfolio/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/react-portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/react-portfolio/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/react-portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/react-portfolio/static/media/react.f370c1e0.png"
+  }
+]);
